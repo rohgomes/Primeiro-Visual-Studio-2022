32aa9ff1ff6c34d5c92366de3f22394af0471508
rohgomes <roberta.cbrecife@gmail.com>
2022-01-04 17:32:43 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Primeiro
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = "World!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
